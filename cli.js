5b220e51678026c107b346fc2308a5747e7e48de
Nikolas Pontikos <npontikos@gmail.com>
2016-07-09 16:38:06 +0100

    cli.js

--- cli.js
+++ cli.js
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
+var monarch=require('./')
 
 var program = require('commander');
 
 program
 .arguments('<file>')
-.option('-i, --id <id>', '')
+.option('-d, --disease_id <id>', '')
 .parse(process.argv);
+
+monarch['getDisease']()
+
+if (typeof program.disease_id != 'undefined') {
+}
+
 
